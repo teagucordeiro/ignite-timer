eaf05a48bbdd81b457f0b297f7869a5e5d7e08c8
Thiago Cordeiro <thiagoty52@gmail.com>
2025-05-18 12:54:08 -0300

    feat: adds library react-router-dom

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from "./Button.stytes";
-
-interface ButtonsProps {
-  variant?: ButtonVariant;
-}
-
-export function Button({ variant = "primary" }: ButtonsProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>;
-}
